8f57b91e4f74b6e52ab51f9c25c55209d0e1053f
Rafael Francisco <franciscorafael814@gmail.com>
2020-07-13 22:33:36 -0400

    chore: sei la

---
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,36 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from '../cryptography/bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hashed_value'))
+  }
+}))
+
+const salt = 12
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt)
+}
+
+describe('Bcrypt Adapter', () => {
+  test('Should calls BcryptAdapter with correct values', async () => {
+    const sut = makeSut()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const sut = makeSut()
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hashed_value')
+  })
+
+  test('Should throws if bcrypt throw', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promisse = sut.encrypt('any_value')
+    await expect(promisse).rejects.toThrow()
+  })
+})
