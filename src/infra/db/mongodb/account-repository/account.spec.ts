3d5fe683b16b1d955dcc17f6dac9f6b5c8ce1e7d
Rafael Francisco <franciscorafael814@gmail.com>
2020-07-14 12:01:48 -0400

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -10,6 +10,11 @@ describe('Account mongo repository', () => {
     await mongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = mongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   const makeSut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
